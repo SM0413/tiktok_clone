07947fe5038e7789fea3222a6cbdbf967a2dbc66
SM0413 <kbjtmdals@naver.com>
2023-02-06 16:58:56 +0900

    Comments

--- lib/screens/features/videos/widgets/video_post.dart
+++ lib/screens/features/videos/widgets/video_post.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/constants/sizes.dart';
 import 'package:tictok_clone/screens/features/videos/widgets/video_button.dart';
+import 'package:tictok_clone/screens/features/videos/widgets/video_comments.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -20,7 +21,7 @@ class VideoPost extends StatefulWidget {
 class _VideoPostState extends State<VideoPost>
     with SingleTickerProviderStateMixin {
   final VideoPlayerController _videoPlayerController =
-      VideoPlayerController.asset("assets/videos/video.mp4");
+      VideoPlayerController.asset("assets/videos/video.MOV");
 
   bool _isPasued = false;
   final Duration _animationDuration = const Duration(milliseconds: 300);
@@ -66,12 +67,14 @@ class _VideoPostState extends State<VideoPost>
   }
 
   void _onVisibilityChanged(VisibilityInfo info) {
-    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+    if (info.visibleFraction == 1 &&
+        !_isPasued &&
+        !_videoPlayerController.value.isPlaying) {
       _videoPlayerController.play();
     }
   }
 
-  void _togglePause() {
+  void _onTogglePause() {
     if (_videoPlayerController.value.isPlaying) {
       _videoPlayerController.pause();
       _animationController.reverse();
@@ -82,6 +85,18 @@ class _VideoPostState extends State<VideoPost>
     setState(() {
       _isPasued = !_isPasued;
     });
+  }
+
+  void _onCommentsTap(BuildContext context) async {
+    if (_videoPlayerController.value.isPlaying) {
+      _onTogglePause();
+    }
+    await showModalBottomSheet(
+      context: context,
+      barrierColor: Colors.transparent,
+      builder: (context) => const VideoComments(),
+    );
+    _onTogglePause();
   }
 
   @override
@@ -100,7 +115,7 @@ class _VideoPostState extends State<VideoPost>
           ),
           Positioned.fill(
             child: GestureDetector(
-              onTap: _togglePause,
+              onTap: _onTogglePause,
             ),
           ),
           Positioned.fill(
@@ -156,8 +171,8 @@ class _VideoPostState extends State<VideoPost>
             bottom: 20,
             right: 10,
             child: Column(
-              children: const [
-                CircleAvatar(
+              children: [
+                const CircleAvatar(
                   radius: 25,
                   backgroundColor: Colors.black,
                   foregroundColor: Colors.white,
@@ -167,11 +182,18 @@ class _VideoPostState extends State<VideoPost>
                   child: Text("Nico"),
                 ),
                 Gaps.v24,
-                VideoButton(icon: FontAwesomeIcons.solidHeart, text: "2.9M"),
+                const VideoButton(
+                    icon: FontAwesomeIcons.solidHeart, text: "2.9M"),
                 Gaps.v24,
-                VideoButton(icon: FontAwesomeIcons.solidComment, text: "33.0M"),
+                GestureDetector(
+                  onTap: () => _onCommentsTap(context),
+                  child: const VideoButton(
+                    icon: FontAwesomeIcons.solidComment,
+                    text: "33.0M",
+                  ),
+                ),
                 Gaps.v24,
-                VideoButton(icon: FontAwesomeIcons.share, text: "Share"),
+                const VideoButton(icon: FontAwesomeIcons.share, text: "Share"),
               ],
             ),
           ),
